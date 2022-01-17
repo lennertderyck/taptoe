852abc77c927171b1067624c3e00b9cd80f890ae
Lennert De Ryck <hello@lennertderyck.be>
2022-01-17 23:54:46 +0100

    Update server.js

--- backend-rest/server.js
+++ backend-rest/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
-// import http from 'http';
-// import { Server } from 'socket.io';
+import http from 'http';
+import { Server } from 'socket.io';
 
 import { REST_PORT } from './constants.js';
 import statusController from './controllers/statusController.js';
@@ -8,8 +8,8 @@ import statusController from './controllers/statusController.js';
 console.log('test')
 
 export const app = express();
-// export const server = http.createServer(app);
-// export const io = new Server(server);
+export const server = http.createServer(app);
+export const io = new Server(server);
 
 app.get('/status', statusController.getStatus);
 
