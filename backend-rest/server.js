6f5122476f6c74e0ccaf63012bbcf5213e96fc95
Lennert De Ryck <hello@lennertderyck.be>
2022-03-06 23:24:50 +0100

    Added cors to rest api

--- backend-rest/server.js
+++ backend-rest/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import cors from 'cors';
 
 import { REST_PORT } from './constants.js';
 import statusController from './controllers/statusController.js';
@@ -10,6 +11,12 @@ console.log('test')
 export const app = express();
 export const server = http.createServer(app);
 export const io = new Server(server);
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept', 'Origin', 'Host', 'Connection', 'Accept-Encoding', 'Accept-Language', 'Cookie'],
+}));
 
 app.get('/status', statusController.getStatus);
 
