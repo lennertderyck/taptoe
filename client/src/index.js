09e9d762bbae66965c60bdc603ac226c2fd73868
Lennert De Ryck <hello@lennertderyck.be>
2022-03-06 23:08:46 +0100

    Allow app init in PWA mode

--- client/src/index.js
+++ client/src/index.js
@@ -17,78 +17,79 @@ import { SplashProvider, AppContextProvider} from './contexts';
 import { ReactComponent as Logo } from './assets/logo.svg';
 import { ReactComponent as LogoJung } from './assets/logos_credits/logo_jung_hinged.svg';
 
-const url = new URL(window.location);
-const forceParam = url.searchParams.get('force');
-const devParam = url.searchParams.get('dev');
-const forcedDevMode = forceParam === '' || forceParam === 'dev' || devParam === '' ? true : false;
+const mqStandAlone = '(display-mode: standalone)';
+const isPWA = navigator.standalone || window.matchMedia('(display-mode: standalone)').matches
+const isDevMode = process.env.NODE_ENV === 'development';
 
 ReactDOM.render(
   <React.StrictMode>
-    { process.env.NODE_ENV === 'production' ? (
-      <div className="h-full w-full flex flex-col items-center justify-center bg-[#E7ECEB]">
-        <div className="flex-1 flex flex-col items-center justify-center p-8">
-          <Logo width="120px" className="mx-auto mb-4" />
-          <h2 className="font-display font-medium text-4xl text-center mb-1 text-tt-emerald-500">Eenvoudig lokalen en terreinen,<br/>huren of verhuren</h2>
-        </div>
-        
-        <div className="pt-8 px-8 pb-6">
-          <Icon name="seedling" size="1.8rem" color="currentColor" className="mx-auto text-tt-emerald-500" />
-          <h2 className="lowercase font-display text-xl text-center text-tt-emerald-700">Nog even geduld</h2>
-          <h3 className="text-center  text-tt-emerald-700">Taptoe is beschikbaar vanaf de zomer van 2022!</h3>
-          
-          <div className="mt-4 text-gray-400 text-center">
-              Design &amp; development door <a href="https://jung.gent?ref=taptoe" target="_blank" rel="noopener">
-                  <LogoJung height="20px" className="inline-block mx-1 text-gray-400" fill="currentColor" />
-              </a> en <a href="https://stefverlinde.be?ref=taptoe" target="_blank" rel="noopener">Stef Verlinde</a>
-          </div>
-        </div>
-      </div>
-    ): (
-      <ApolloProvider client={client}>
-        <HelpProvider>
-        <BrowserRouter>
-          <AuthProvider>
-            <AppContextProvider>
-              <DevToolsBar />
-              <SplashProvider>
-                <Routes>
-                  <Route path="/" element={<App />} />
-                    <Route path="account" element={<AccountModule />}>
-                  </Route>
-                  <Route path="account/login" element={<LoginModule />} />
-                  <Route path="tribes" element={<TribeModule />}>
-                    <Route path="nieuw" element={<NewTribeModule />}/>
-                    <Route path=":id" element={<TribeDetailModule />}/>
-                  </Route>
-                  <Route path="locaties" element={<LocationModule />}>
-                    <Route path="nieuw/:tribeId" element={<NewLocationModule />} />
-                    <Route path="nieuw" element={<NewLocationModule />} />
-                  </Route>
-                  <Route path="locaties/:id" element={<LocationDetailModule />} />
-                  <Route path="locaties/:id/edit" element={<LocationEditModule />} />
-                  <Route path="manage" element={<ManageModule />}>
-                    <Route index element={<ManageOverviewModule />} />
-                    <Route path="*" element={<ManageWrapperModule />}>
-                      <Route path="tribes" element={<ManageTribesModule />} />
-                      <Route path="users">
-                        <Route index element={<ManageUsersModule />} />
-                        <Route path="roles" element={<ManageUserRolesModule />} />
-                        <Route path="auth-scopes" element={<AuthScopesModule />} />
+    {
+      isPWA || isDevMode ? (
+        <ApolloProvider client={client}>
+          <HelpProvider>
+          <BrowserRouter>
+            <AuthProvider>
+              <AppContextProvider>
+                <DevToolsBar />
+                <SplashProvider>
+                  <Routes>
+                    <Route path="/" element={<App />} />
+                      <Route path="account" element={<AccountModule />}>
+                    </Route>
+                    <Route path="account/login" element={<LoginModule />} />
+                    <Route path="tribes" element={<TribeModule />}>
+                      <Route path="nieuw" element={<NewTribeModule />}/>
+                      <Route path=":id" element={<TribeDetailModule />}/>
+                    </Route>
+                    <Route path="locaties" element={<LocationModule />}>
+                      <Route path="nieuw/:tribeId" element={<NewLocationModule />} />
+                      <Route path="nieuw" element={<NewLocationModule />} />
+                    </Route>
+                    <Route path="locaties/:id" element={<LocationDetailModule />} />
+                    <Route path="locaties/:id/edit" element={<LocationEditModule />} />
+                    <Route path="manage" element={<ManageModule />}>
+                      <Route index element={<ManageOverviewModule />} />
+                      <Route path="*" element={<ManageWrapperModule />}>
+                        <Route path="tribes" element={<ManageTribesModule />} />
+                        <Route path="users">
+                          <Route index element={<ManageUsersModule />} />
+                          <Route path="roles" element={<ManageUserRolesModule />} />
+                          <Route path="auth-scopes" element={<AuthScopesModule />} />
+                        </Route>
                       </Route>
                     </Route>
-                  </Route>
-                  <Route path="*" element={<div>Not found</div>} />
-                </Routes>
-              </SplashProvider>
-              
-              <HelpSidebar />
-              {/* <SpotlightSearch /> */}
-            </AppContextProvider>
-          </AuthProvider>
-        </BrowserRouter>
-        </HelpProvider>
-      </ApolloProvider>
-    )}
+                    <Route path="*" element={<div>Not found</div>} />
+                  </Routes>
+                </SplashProvider>
+                
+                <HelpSidebar />
+                {/* <SpotlightSearch /> */}
+              </AppContextProvider>
+            </AuthProvider>
+          </BrowserRouter>
+          </HelpProvider>
+        </ApolloProvider>
+      ) : (
+        <div className="h-full w-full flex flex-col items-center justify-center bg-[#E7ECEB]">
+          <div className="flex-1 flex flex-col items-center justify-center p-8">
+            <Logo width="120px" className="mx-auto mb-4" />
+            <h2 className="font-display font-medium text-4xl text-center mb-1 text-tt-emerald-500">Eenvoudig lokalen en terreinen,<br/>huren of verhuren</h2>
+          </div>
+          
+          <div className="pt-8 px-8 pb-6">
+            <Icon name="seedling" size="1.8rem" color="currentColor" className="mx-auto text-tt-emerald-500" />
+            <h2 className="lowercase font-display text-xl text-center text-tt-emerald-700">Nog even geduld</h2>
+            <h3 className="text-center  text-tt-emerald-700">Taptoe is beschikbaar vanaf de zomer van 2022!</h3>
+            
+            <div className="mt-4 text-gray-400 text-center">
+                Design &amp; development door <a href="https://jung.gent?ref=taptoe" target="_blank" rel="noopener">
+                    <LogoJung height="20px" className="inline-block mx-1 text-gray-400" fill="currentColor" />
+                </a> en <a href="https://stefverlinde.be?ref=taptoe" target="_blank" rel="noopener">Stef Verlinde</a>
+            </div>
+          </div>
+        </div>
+      )
+    }
   </React.StrictMode>,
   document.getElementById('root')
 );
